a6e24a186a0f2edcd8abc8cd591ceb3dd5ce86ec
Noel Leahy <nleahy1234@gmail.com>
2021-11-10 18:18:55 +0000

    Added upcoming movies.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
-
+import UpComingMoviePage from "./pages/upComingMoviesPage";
 
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
 
@@ -15,10 +15,12 @@ const App = () => {
     <BrowserRouter>
         <SiteHeader />      {/* New Header  */}
       <Switch>
+      <Route exact path="/movies/upcoming" component={UpComingMoviePage} />
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
         <Route path="/reviews/:id" component={MovieReviewPage} />
-        <Route path="/movies/:id" component={MoviePage} />
+        
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/movies/:id" component={MoviePage} />
         <Redirect from="*" to="/" />
       </Switch>
     </BrowserRouter>
