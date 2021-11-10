79509951aff2f6473b425091a3c4d1a7ce1abe0d
Noel Leahy <nleahy1234@gmail.com>
2021-11-10 18:02:23 +0000

    Full review page.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
+import MovieReviewPage from "./pages/movieReviewPage";
 
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
 
@@ -20,6 +21,7 @@ const App = () => {
       </ul>
       <Switch>
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+        <Route path="/reviews/:id" component={MovieReviewPage} />
         <Route path="/movies/:id" component={MoviePage} />
         <Route exact path="/" component={HomePage} />
         <Redirect from="*" to="/" />
